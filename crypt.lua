ac866ff64da0f32eccac3a69e7261691f3447489
gitb0i <64463178+gitb0i@users.noreply.github.com>
2022-12-30 04:20:20 -0800

    Update crypt.lua
    Added encrypt

--- crypt.lua
+++ crypt.lua
@@ -1,5 +1,33 @@
 local module = {}
 
+function module.encrypt(msg,code)
+
+	local nstr = ""
+	local ncode = ''
+	
+	for _,v in pairs( string.split(code,'') ) do
+		ncode = ncode.. string.byte(v)
+	end
+	
+
+	for i,v in pairs( string.split(msg,'') ) do
+		
+		local id = string.byte(v)*(i)
+		id *= math.sin( tonumber(string.sub(ncode,1,1) )/ncode^.5 )*(tonumber(string.sub(ncode,2,2) )/2)
+		id += math.cos( tonumber(string.sub(ncode,4,4) )/ncode^.5 )
+		
+		nstr = nstr.. id..'\\'
+
+	end
+
+	
+
+	return(nstr)
+
+
+end
+
+
 function module.decrypt(msg,code)
 
 	local nstr = ''
@@ -8,19 +36,19 @@ function module.decrypt(msg,code)
 	for _,v in pairs( string.split(code,'') ) do
 		ncode = ncode.. string.byte(v)
 	end
-
-
+	
+	
 
 	for i,v in pairs( string.split(msg,'\\') ) do
 
 		pcall(function()
-
-
-
+			
+			
+			
 			v -= math.cos( tonumber(string.sub(ncode,4,4) )/ncode^.5 )
 			v /= math.sin( tonumber(string.sub(ncode,1,1) )/ncode^.5 )*(tonumber(string.sub(ncode,2,2) )/2)
 			v = math.round(tonumber(v)/i)
-
+			
 			nstr = nstr.. string.char(v)
 		end)
 
@@ -31,4 +59,3 @@ function module.decrypt(msg,code)
 end
 
 return module
-
